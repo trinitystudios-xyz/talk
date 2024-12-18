f5d0a009c7e31ce03ca4ebb79e48c2ac64c0fa3b
ZyphenVisuals <rotarujunior@gmail.com>
2024-12-18 14:46:34 +0200

    change color theme again

--- src/main.js
+++ src/main.js
@@ -12,17 +12,17 @@ import ToastService from 'primevue/toastservice'
 const T3Theme = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '{violet.50}',
-      100: '{violet.100}',
-      200: '{violet.200}',
-      300: '{violet.300}',
-      400: '{violet.400}',
-      500: '{violet.500}',
-      600: '{violet.600}',
-      700: '{violet.700}',
-      800: '{violet.800}',
-      900: '{violet.900}',
-      950: '{violet.950}',
+      50: '{rose.50}',
+      100: '{rose.100}',
+      200: '{rose.200}',
+      300: '{rose.300}',
+      400: '{rose.400}',
+      500: '{rose.500}',
+      600: '{rose.600}',
+      700: '{rose.700}',
+      800: '{rose.800}',
+      900: '{rose.900}',
+      950: '{rose.950}',
     },
   },
 })
