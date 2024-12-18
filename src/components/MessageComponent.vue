b055401b372841278f5e34a2ade01ba1bb9dc6bd
ZyphenVisuals <rotarujunior@gmail.com>
2024-12-18 13:54:28 +0200

    fix messages in light mode

--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -19,12 +19,12 @@ onMounted(() => {
 
 <template>
   <div class="p-4 flex" :class="{ 'justify-end': props.own }">
-    <div class="bg-surface-800 px-6 py-4 rounded-lg">
+    <div class="bg-surface-100 dark:bg-surface-800 px-6 py-4 rounded-lg">
       <h1 class="text-primary inline-block text-lg">{{ props.msg.expand.from.name }}</h1>
       <p class="text-gray-500 inline-block ml-2 text-sm">
         {{ timeStyle.format(Date.parse(props.msg.created)) }}
       </p>
-      <div class="prose prose-invert" ref="msgContainer"></div>
+      <div class="prose dark:prose-invert" ref="msgContainer"></div>
     </div>
   </div>
 </template>
